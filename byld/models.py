e7fffbbcd36cc0f0b51cde2428236cebe58be763
Kushagra Singh <singh.kushagra.1996@gmail.com>
2015-09-28 18:30:42 +0530

    Added leaderboard

--- byld/models.py
+++ byld/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Team(models.Model):
-	
+
 	team = models.ForeignKey(User)
 	score = models.FloatField(default = 0.0)
-
-	def __unicode__(self):
-		return self.title
