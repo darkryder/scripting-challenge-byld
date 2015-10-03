83fc25c721b9206320af88a99800e46a83a4865f
Sambhav Satija <sambhav13085@iiitd.ac.in>
2015-10-04 03:10:25 +0530

    Added Rapid Fire Question

---
+++ Qrapidfire/server.py
@@ -0,0 +1,145 @@
+__author__ = 'darkryder (sambhav13085@iiitd.ac.in)'
+
+# Type of questions:
+# 3 levels
+#
+# 1 level
+# What is the gcd of (x, y) where x-> [1, 10^6]; y -> [1, 10^6]
+# Number of factors of x where x -> [1, 10^6]
+# This is sparta / Answer to life / universe and everything
+#
+# 2nd level
+# Math calculation -> an expression with 8 variables and operators and brackets
+#
+# 3rd level
+# For this following video, what is it's rank on youtube most watched?
+# Do you love Byld? [yes]
+
+import SocketServer as SS
+from threading import Thread
+from time import time as now
+from random import choice, randint
+
+from utils import gcd, number_of_factors, math_equation_QA_generator
+from utils import gcd_q_generator, number_of_factors_q_generator, youtube_text_QA_generator
+
+PORT = 5021
+
+class Question(object):
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+    def __repr__(self):
+        return "<%s>: %s" % (str(self.question), str(self.answer))
+
+    def verify(self, check):
+        return self.answer in [check, check.strip(), check.strip().lower()]
+
+class NaiveMathQuestion(Question):
+    def __init__(self, q_maker_func, answer_func, *params):
+        question = q_maker_func(*params)
+        answer = answer_func(*params)
+        super(NaiveMathQuestion, self).__init__(question, answer)
+
+    def verify(self, check):
+        try:
+            return self.answer == int(check)
+        except Exception, e:
+            print e
+            return False
+
+class MathEquationQuestion(NaiveMathQuestion):
+    def __init__(self, *args):
+        question, answer = math_equation_QA_generator(*args)
+        super(MathEquationQuestion, self).__init__(
+            lambda x: question,
+            lambda x: answer,
+            None
+            )
+
+levels_to_questions = {1: [], 2: [], 3:[], 4:[]}
+
+for _ in xrange(1, 25):
+    r_ = lambda : randint(100, 10**5)
+
+    q = NaiveMathQuestion(gcd_q_generator, gcd, r_(), r_())
+    print "Created Question", q
+    levels_to_questions[1].append(q)
+
+    q = NaiveMathQuestion(number_of_factors_q_generator, number_of_factors, r_())
+    print "Created Question", q
+    levels_to_questions[1].append(q)
+
+    temp_args = lambda l, u, count: [randint(l, u) for _ in xrange(count)]
+    q = MathEquationQuestion(temp_args(10, 1000, 8))
+    print "Created Question", q
+    levels_to_questions[2].append(q)
+
+for song_title, rank in youtube_text_QA_generator():
+    q = Question('What is the youtube most watched rank of "%s"' % song_title,
+        str(rank))
+    print "Created Question", q
+    levels_to_questions[3].append(q)
+
+levels_to_questions[1].append(Question("What is the answer to Life, Universe"
+    " and Everything?", "42"))
+print "Added a spark of life."
+
+levels_to_questions[1].append(Question('What number do you think of when'
+    ' you hear "This is Sparta!"', "300"))
+print "Added motivation."
+
+levels_to_questions[4].append(Question("Do you love Byld? [yes/no]",
+    "yes"))
+print "Added some love"
+
+print """
+Questions Ready.
+Let's Roll.
+Server up on port %d
+""" % PORT
+
+class QuizMaster(SS.BaseRequestHandler):
+    MAX_UPPER_TIME = 5 # seconds
+    FLAG = "8b433670258f79578f9a4e5ea388b007"
+    def handle(self):
+        current_level = 1
+        last_request_time = now()
+        answer = 'anything'
+        print "[%s] Guinea pig connected." % str(self.client_address)
+
+        while len(answer):
+
+            self.request.send("Level %d.\n" % current_level)
+            question = choice(levels_to_questions[current_level])
+            self.request.send(str(question.question) + '\n')
+
+            answer = self.request.recv(2048)
+            diff = now() - last_request_time
+            last_request_time = now()
+            if diff > QuizMaster.MAX_UPPER_TIME:
+                print "[%s] Reply time exceeded: %s" % (str(self.client_address), str(diff))
+                self.request.send("Too late.\n")
+                break
+
+            if not question.verify(answer):
+                print "[%s] Incorrect answer for Question [%s]. Received answer [%s]" % (
+                    str(self.client_address), str(question), str(answer))
+                self.request.send("Incorrect answer bruh.\n")
+                break
+
+            current_level += 1
+
+            if current_level == 5:
+                print "[%s] pig escaped." % str(self.client_address)
+                self.request.send("You won: [%s]\n" % QuizMaster.FLAG)
+                break
+
+        print "[%s] pig died. :(" % str(self.client_address)
+        self.request.close()
+
+class ThreadedQuizMaster(SS.ThreadingMixIn, SS.TCPServer):
+    pass
+
+ThreadedQuizMaster(('0.0.0.0', PORT), QuizMaster).serve_forever()
---
+++ Qrapidfire/utils.py
@@ -0,0 +1,102 @@
+__author__ = 'darkryder (sambhav13085@iiitd.ac.in)'
+
+
+from __future__ import division
+from math import sqrt, ceil
+from random import shuffle, choice
+
+_rbool = lambda: choice([True, False])
+
+def gcd(*args):
+    a, b = args
+    a, b = max(a, b), min(a, b)
+    while a % b != 0 and b != 1:
+        a, b = b, a % b
+    return b
+
+def number_of_factors(number):
+    count = 0
+    for i in xrange(1, int(sqrt(number)) + 1): # not including sqrt
+        if number % i == 0:
+            count += 2
+    if sqrt(number) == int(sqrt(number)): count -= 1
+    return count
+
+def math_equation_QA_generator(*args):
+    _ARGS_LENGTH = 8
+    args = args[0]
+    assert len(args) == _ARGS_LENGTH
+    assert 0 not in args
+    operators = ['+', '-', '/', '*']
+    args = list(args)
+    shuffle(args)
+
+    # QUESTION = "%d %s %d %s %d %s %d %s %d %s %d %s %d %s %d"
+    # only open brackets in the first half of the equation
+    QUESTION = []
+    opened_brackets_count = 0
+
+    for index, arg in enumerate(args):
+
+        # time to insert a number. Also a bracket can be opened.
+        if index <= _ARGS_LENGTH / 2 and _rbool():
+            QUESTION.append('(')
+            opened_brackets_count += 1
+        QUESTION.append(str(arg))
+
+        # time to insert an operator. Also a bracket can be closed.
+        if opened_brackets_count > 0 and _rbool():
+            QUESTION.append(')')
+            opened_brackets_count -= 1
+        QUESTION.append(choice(operators))
+
+    QUESTION = QUESTION[:-1]
+    for _ in xrange(opened_brackets_count): QUESTION.append(')')
+
+    QUESTION = ''.join(QUESTION)
+    ANSWER = int(eval(QUESTION))
+    info = ("While evaluating the given equation, understand that 1/2 = 0.5"
+        "\nFinally give the int() of the answer.\n")
+
+    return info + "What is the value of " + QUESTION, ANSWER
+
+
+def gcd_q_generator(*params):
+    assert len(params) == 2
+    assert 0 not in params
+    TEXTS = [
+        "What is the gcd of %d and %d",
+        "Find the gcd of %d and %d"
+        ]
+    return choice(TEXTS) % tuple(params)
+
+def number_of_factors_q_generator(*params):
+    assert len(params) == 1
+    assert 0 not in params
+    TEXTS = [
+        "Find the number of factors of %d",
+        "How many factors are there of %d"
+    ]
+    return choice(TEXTS) % tuple(params)
+
+def youtube_text_QA_generator():
+    mapper = [
+        "Gangnam Style",
+        "Baby",
+        "Blank Space",
+        "Dark Horse",
+        "Roar",
+        "Shake It Off",
+        "Bailando",
+        "All About That Bass",
+        "Uptown Funk",
+        "See You Again",
+        "Counting Stars",
+        "Party Rock Anthem",
+        "Love the Way You Lie",
+        "Waka Waka (This Time for Africa)",
+        "Love the Way You Lie"
+    ]
+
+    for i, x in enumerate(mapper):
+        yield x, i+1
